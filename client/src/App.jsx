fdc7c60c201d4904fa334a27086943ee0ac64048
kritwatchara wangkhumphai <jumb3839@kritwatcharas-MacBook-Air.local>
2023-09-01 16:30:52 +0700

    fix: add register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserRegisterPage1 from "./pages/UserRegisterPage1.jsx";
-import "./App.css";
+import UserRegisterPage2 from "./pages/UserRegisterPage2.jsx";
+import UserRegisterPage3 from "./pages/UserRegisterPage3.jsx";
 import LoginPage from "./pages/UserLoginPage.jsx";
+import "./App.css";
 
 function App() {
   const NoMatch = () => <h1>Page Not Found.</h1>;
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/user/register1" element={<UserRegisterPage1 />} />
+          <Route path="/user/register2" element={<UserRegisterPage2 />} />
+          <Route path="/user/register3" element={<UserRegisterPage3 />} />
           <Route path="/user/login" element={<LoginPage />} />
           <Route path="*" element={<NoMatch />} />
         </Routes>
