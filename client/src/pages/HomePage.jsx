ad844d8fb473a17e1ceb255de15224a03abb8268
Keem <mr.sahapap@gmail.com>
2023-09-05 14:13:32 +0700

    feat: create layout landing page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+function Homepage() {
+  return (
+    <>
+      <div className="bg-white h-[64px]">nav</div>
+      <div className="bg-gray-600 h-[833px]">
+        The place where you get that job
+      </div>
+      <div className="flex  flex-row h-[472px]">
+        <div className="bg-pink-800 grow-[6]">Find your next job</div>
+        <div className="bg-white grow-[4]">pic</div>
+      </div>
+      <div className="bg-gray-700 h-[537px]">
+        <div className="h-[486px]">footer</div>
+        <hr />
+        <div className="h-[51px]">2021</div>
+      </div>
+    </>
+  );
+}
+
+export default Homepage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+export default {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-	],
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
   theme: {
     container: {
       center: true,
@@ -73,4 +74,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
