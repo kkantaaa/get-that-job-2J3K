af84e23e28bb141714aac0b4de39eb1c48cdb5f1
jashleyx <patricia.singha@outlook.com>
2023-09-05 14:12:25 +0700

    fix: edit PersonalInformation component

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,31 +1,17 @@
 import { createContext, useContext, useState } from "react";
 import axios from "axios";
 
-import React from "react";
-// import jwtDecode from "jwt-decode";
-// แก้ไข authentication
-
-
 const AuthContext = createContext();
-
 
 // eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   return useContext(AuthContext);
 }
 
-
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
-  // const userlogin = async (data) => {
-  //   const result = await axios.post("http://localhost:3000/auth/login", data);
-  //   const token = result.data.token;
-  //   const userDataFromToken = jwtDecode(token);
-  //   setState({ ...state, user: userDataFromToken });
-  // };
-  
   const UserRegister = async (data) => {
     try {
       await axios.post("http://localhost:your-port/register", data);
@@ -34,12 +20,23 @@ export const AuthProvider = ({ children }) => {
       setUserData(data);
     } catch (error) {
       console.error("Error: unable to register the account", error);
+    }
+  };
 
+  const RecruiterRegister = async (data) => {
+    try {
+      await axios.post("http://localhost:your-port/register", data);
+      console.log("Registration successful");
+
+      setUserData(data);
+    } catch (error) {
+      console.error("Error: unable to register the account", error);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ userData, UserRegister }}>
+    <AuthContext.Provider value={{ userData, UserRegister, RecruiterRegister }}>
       {children}
     </AuthContext.Provider>
   );
+};
