24b4e94f57e6b542d21758178a006f39ebcce02f
kkantaaa <kantavanit@gmail.com>
2023-09-06 10:19:09 +0700

    style: Style user login page

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useForm, Controller } from "react-hook-form";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function LoginForm() {
   const navigate = useNavigate();
@@ -9,30 +9,37 @@ export default function LoginForm() {
     formState: { errors },
   } = useForm();
   const onSubmit = () => {
-    navigate("/"); //not yet assign path to next page
+    console.log(control._fields.email._f.value); //not yet assign path to next page
   };
-
+ 
   return (
-    <div className="login_box">
-      <div className="role">
-        <button value="professional">
+    <div className="flex flex-col justify-between text-left text-[12px] h-2/3">
+      <div className="text-[14px] w-fit ">
+        <button className="mr-4 underline decoration-sky-500 underline-offset-8">
           <Link to="/user/login">PROFESSIONAL</Link>
         </button>
-        <button value="recuiter">
+        <button className="mr-4 underline decoration-sky-500 underline-offset-8">
           <Link to="/recuiter/login">RECRUITER</Link>
         </button>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="email_input">
-          <label htmlFor="email">
-            EMAIL
+
+      
+        <form className="h-5/6 flex flex-col justify-between" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col ">
+            <label className="w-fit text-[10px]" htmlFor="email">
+              EMAIL
+            </label>
             <Controller
               name="email"
               control={control}
               defaultValue=""
-              rules={{ required: "Email is required" }}
+              rules={{
+                required:
+                  "The email address you entered isn't connected to an account.",
+              }}
               render={({ field }) => (
                 <input
+                  className="h-[36px] rounded-[8px] text-[14px] pl-[8px] border-solid border-[1px] border-sky-500"
                   id="email"
                   type="email"
                   placeholder="some.user@mail.com"
@@ -40,20 +47,23 @@ export default function LoginForm() {
                 />
               )}
             />
-          </label>
-          <span>{errors.email && errors.email.message}</span>
-        </div>
+            <span>{errors.email && errors.email.message}</span>
+          </div>
 
-        <div className="password_input">
-          <label htmlFor="password">
-            PASSWORD
+          <div className="flex flex-col">
+            <label className="w-fit text-[10px]" htmlFor="password">
+              PASSWORD
+            </label>
             <Controller
               name="password"
               control={control}
               defaultValue=""
-              rules={{ required: "Password is required" }}
+              rules={{
+                required: "The password that you've entered is incorrect.",
+              }}
               render={({ field }) => (
                 <input
+                  className="h-[36px] rounded-[8px] text-[14px] pl-[8px] border-solid border-[1px] border-sky-500"
                   id="password"
                   type="password"
                   placeholder="******"
@@ -61,12 +71,15 @@ export default function LoginForm() {
                 />
               )}
             />
-          </label>
-          <span>{errors.password && errors.password.message}</span>
-        </div>
-
-        <input type="submit" />
-      </form>
+            <span>{errors.password && errors.password.message}</span>
+          </div>
+          <div className="text-right">
+            <button className="w-[80px] h-[40px] px-[16px] py-[8px] bg-slate-50 rounded-[16px] text-[14px]" type="submit">
+              LOGIN
+            </button>
+          </div>
+        </form>
+      
     </div>
   );
 }
--- client/src/pages/UserLoginPage.jsx
+++ client/src/pages/UserLoginPage.jsx
@@ -1,14 +1,22 @@
 import LoginForm from "../components/LoginForm";
+
 function UserLoginPage() {
   return (
-    <div className="loginPage">
-      <div className="login-container">
-        <div className="nav"></div>
-        <div className="loginForm">
-          <div className="greeting">Welcome back</div>
-          <div>Login to you account as...</div>
+    <div className="w-screen h-screen bg-red-200">
+      <div className="nav"></div>
+
+      <div className="flex justify-evenly">
+
+        <div className="flex flex-col h-[359px] w-[359px] justify-between">
+          <h3 className="text-5xl w-fit">Welcome back</h3>
+          <h6 className="text-xl w-fit">Login to you account as...</h6>
           <LoginForm />
         </div>
+
+        <div className="w-[560px]">
+          <img src="https://i.kym-cdn.com/photos/images/original/000/011/268/560px-No_moar_dog_drawn.jpg" />
+        </div>
+
       </div>
     </div>
   );
