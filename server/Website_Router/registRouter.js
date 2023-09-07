a49850fb9b97ab4b5cdbc92a2cfe8b6126efead0
jashleyx <patricia.singha@outlook.com>
2023-09-07 17:35:52 +0700

    commit changes

--- server/Website_Router/registRouter.js
+++ server/Website_Router/registRouter.js
@@ -6,21 +6,18 @@ import multer from "multer";
 const registRouter = Router();
 const multerUpload = multer({ dest: "../uploads/" });
 
-// Routes for testing database connection
+// Test database connection
 registRouter.get("/test/get_tabledata", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM testregist");
-    return res.json({
-      message: result.rows,
-    });
+    const result = await pool.query("select * from testregist");
+    return res.json({ message: result.rows });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({
-      message: "Error fetching data from the database",
-    });
+    return res.status(500).json({ message: "Error fetching data from the database" });
   }
 });
 
+// Test insert data to the database
 registRouter.post("/test/post_tabledata", async (req, res) => {
   try {
     const user = {
@@ -32,6 +29,7 @@ registRouter.post("/test/post_tabledata", async (req, res) => {
       title: req.body.title,
       experience: req.body.experience,
     };
+    
     const salt = await bcrypt.genSalt(14);
     user.password = await bcrypt.hash(user.password, salt);
 
@@ -47,35 +45,16 @@ registRouter.post("/test/post_tabledata", async (req, res) => {
         user.experience,
       ]
     );
-    
-    return res.json({
-      message: "Get that job account created!",
-    });
+
+    return res.json({ message: "Get that job account created!" });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({
-      message: "Error! Please try registering again.",
-    });
+    return res.status(500).json({ message: "Error creating the account" });
   }
 });
 
-// Routes for professional users
-registRouter.get("/professional", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM UserTable");
-    return res.json({
-      message: result.rows,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      message: "Error fetching data from the database",
-    });
-  }
-});
-
+// Insert data to the UserTable for professional users
 registRouter.post("/professional", async (req, res) => {
-  console.log(req.body);
   try {
     const user = {
       email: req.body.email,
@@ -90,12 +69,12 @@ registRouter.post("/professional", async (req, res) => {
       havefile: req.body.havefile,
       confirmedpassword: req.body.confirmedpassword,
     };
+    
     const salt = await bcrypt.genSalt(14);
     user.password = await bcrypt.hash(user.password, salt);
 
     await pool.query(
-
-      "insert into UserTable (email,password,name,phone,birthdate,linkedin,title,jobExp,education,havefile,confirmedpassword) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+      "insert into UserTable (email,password,name,phone,birthdate,linkedin,title,jobexp,education,havefile,confirmedpassword) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
       [
         user.email,
         user.password,
@@ -116,27 +95,11 @@ registRouter.post("/professional", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({
-      message: "Error! Please try registering again.",
-    });
+    return res.status(500).json({ message: "Error creating the account" });
   }
 });
 
-// Routes for recruiter users
-registRouter.get("/recruiter", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM recruitertable");
-    return res.json({
-      message: result.rows,
-    });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      message: "Error fetching data from the database",
-    });
-  }
-});
-
+// Insert data to the recruitertable for recruiter users
 registRouter.post("/recruiter", async (req, res) => {
   try {
     const user = {
@@ -147,6 +110,7 @@ registRouter.post("/recruiter", async (req, res) => {
       aboutcompany: req.body.aboutcompany,
       havefile: req.body.havefile,
     };
+    
     const salt = await bcrypt.genSalt(14);
     user.companypassword = await bcrypt.hash(user.companypassword, salt);
 
@@ -167,9 +131,7 @@ registRouter.post("/recruiter", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({
-      message: "Error! Please try registering again.",
-    });
+    return res.status(500).json({ message: "Error creating the account" });
   }
 });
 
