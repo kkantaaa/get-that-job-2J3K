d8328bb683496db482bb70af05fbf9c6a08254a6
Keem <mr.sahapap@gmail.com>
2023-09-07 10:13:41 +0700

    feat: adding design all section in landing-page

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -9,18 +9,19 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-Pink text-white hover:bg-LightPink active:bg-DarkPink",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border  border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-white text-Gray border border-Pink text-Gray hover:bg-Background active:bg-Background-foreground",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-14 px-[24px] py-[16px] m-[4px] rounded-2xl",
+        secondary: "h-10 px-[16px] py-[8px] rounded-2xl",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
