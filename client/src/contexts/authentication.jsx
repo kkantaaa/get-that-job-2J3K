edbf7d3fb089d4220bf573dc69bbe7c8653a9df4
kritwatchara wangkhumphai <jumb3839@kritwatcharas-MacBook-Air.local>
2023-09-07 16:07:05 +0700

    fix: implement user and recruiter login

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -9,49 +9,41 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState(null);
 
-  try {
-    const UserLogin = async (data) => {
-      console.log("from authentication");
-      console.log(data);
-      const result = await axios.post(
-        "http://localhost:4000/auth/user/login",
-        data
-      );
-      const token = result.data.token;
-      localStorage.setItem("token", token);
-      const userDataFromToken = jwtDecode(token);
-      console.log("this is token");
-      console.log(token);
-      setUserData({ userDataFromToken });
-      console.log("this is userData");
-      console.log(userData);
-      navigate("/");
-    };
-  } catch (error) {
-    console.error("Error: unable to login", error);
-  }
+  const UserLogin = async (data) => {
+    console.log("from authentication");
+    console.log(data);
+    const result = await axios.post(
+      "http://localhost:4000/auth/user/login",
+      data
+    );
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    const userDataFromToken = jwtDecode(token);
+    console.log("this is token");
+    console.log(token);
+    setUserData({ userDataFromToken });
+    console.log("this is userData");
+    console.log(userData);
+    navigate("/");
+  };
 
-  try {
-    const RecruiterLogin = async (data) => {
-      console.log("from authentication");
-      console.log(data);
-      const result = await axios.post(
-        "http://localhost:4000/auth/recruiter/login",
-        data
-      );
-      const token = result.data.token;
-      localStorage.setItem("token", token);
-      const userDataFromToken = jwtDecode(token);
-      console.log("this is token");
-      console.log(token);
-      setUserData({ userDataFromToken });
-      console.log("this is userData");
-      console.log(userData);
-      navigate("/");
-    };
-  } catch (error) {
-    console.error("Error: unable to login", error);
-  }
+  const RecruiterLogin = async (data) => {
+    console.log("from authentication");
+    console.log(data);
+    const result = await axios.post(
+      "http://localhost:4000/auth/recruiter/login",
+      data
+    );
+    const token = result.data.token;
+    localStorage.setItem("token", token);
+    const userDataFromToken = jwtDecode(token);
+    console.log("this is token");
+    console.log(token);
+    setUserData({ userDataFromToken });
+    console.log("this is userData");
+    console.log(userData);
+    navigate("/");
+  };
 
   const UserRegister = async (data) => {
     try {
