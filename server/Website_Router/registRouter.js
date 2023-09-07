35f86b03b47566cc4e2bc40cd10a786d61673fa7
jashleyx <patricia.singha@outlook.com>
2023-09-07 17:35:52 +0700

    commit changes

--- server/Website_Router/registRouter.js
+++ server/Website_Router/registRouter.js
@@ -68,7 +68,7 @@ registRouter.get("/professional", async (req, res) => {
 });
 //Postman - "Post" "localhost:4000/regist/professional"
 registRouter.post("/professional", async (req, res) => {
-  console.log("check table");
+  console.log(req.body);
   try {
     const user = {
       email: req.body.email,
@@ -81,12 +81,13 @@ registRouter.post("/professional", async (req, res) => {
       jobExp: req.body.jobExp,
       education: req.body.education,
       havefile: req.body.havefile,
+      confirmedpassword: req.body.confirmedpassword,
     };
     const salt = await bcrypt.genSalt(14);
     user.password = await bcrypt.hash(user.password, salt);
 
     await pool.query(
-      "insert into UserTable (email,password,name,phone,birthdate,linkedin,title,jobExp,education,havefile) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)",
+      "insert into UserTable (email,password,name,phone,birthdate,linkedin,title,jobExp,education,havefile,confirmedpassword) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
 
       [
         user.email,
@@ -99,6 +100,7 @@ registRouter.post("/professional", async (req, res) => {
         user.jobExp,
         user.education,
         user.havefile,
+        user.confirmedpassword,
       ]
     );
     console.log(user);
