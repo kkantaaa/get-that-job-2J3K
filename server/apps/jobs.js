876c5bd3242dc8020e1521a9696799791502be9d
kkantaaa <kantavanit@gmail.com>
2023-09-11 15:13:30 +0700

    feat: added access job_type from req.body

--- server/apps/jobs.js
+++ server/apps/jobs.js
@@ -72,15 +72,14 @@ jobRouter.get("/:id", async (req, res) => {
 });
 
 jobRouter.post("/", async (req, res) => {
-    console.log("Request Body:", req.body);
-    
+  console.log("Request Body:", req.body);
 
   try {
-      const job = {
-        //use param to get recruiter_id
+    const job = {
+      //use param to get recruiter_id
       job_title: req.body.job_title,
       category: req.body.category, //use category replace category_name
-      //type
+      type: req.body.type, //use type replace type_name
       salary_min: req.body.salary_min,
       salary_max: req.body.salary_max,
       about_job_position: req.body.about_job_position,
@@ -97,11 +96,21 @@ jobRouter.post("/", async (req, res) => {
       return res.status(410).json({ message: "Category not found" });
     }
 
+    const typeQuery = await pool.query(
+      "SELECT * FROM job_types WHERE type_name = $1",
+      [job.type]
+    );
+    console.log("type Query Result:", typeQuery.rows);
+    if (typeQuery.rows.length === 0) {
+      return res.status(411).json({ message: "type not found" });
+    }
+
     await pool.query(
-      "insert into jobs (job_title,job_category_id,salary_min,salary_max,about_job_position,mandatory_requirement,optional_requirement) values ($1,$2,$3,$4,$5,$6,$7)",
+      "insert into jobs (job_title,job_category_id,job_type_id,salary_min,salary_max,about_job_position,mandatory_requirement,optional_requirement) values ($1,$2,$3,$4,$5,$6,$7,$8)",
       [
         job.job_title,
         parseInt(categoryQuery.rows[0].job_category_id, 10),
+        parseInt(typeQuery.rows[0].job_type_id, 10),
         job.salary_min,
         job.salary_max,
         job.about_job_position,
