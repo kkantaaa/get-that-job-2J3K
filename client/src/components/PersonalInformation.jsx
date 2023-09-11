a0e5b5f09cd0f1426db3605236a7802579c89c14
jashleyx <patricia.singha@outlook.com>
2023-09-11 10:10:24 +0700

    fix PersonalInformation components

--- client/src/components/PersonalInformation.jsx
+++ client/src/components/PersonalInformation.jsx
@@ -1,36 +1,49 @@
 import { useForm, Controller } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/authentication";
 import { useGlobalContext } from "@/contexts/registerContexts";
 import { useEffect } from "react";
+<<<<<<< HEAD
 import { useAuth } from "@/contexts/authentication";
+=======
+>>>>>>> a12ecf1 (fix PersonalInformation components)
 
-function PersonalInformation() {
-  const { userData, setUserData } = useGlobalContext();
-  const { UserRegister } = useAuth();
+function ProfessionalInfo() {
   const navigate = useNavigate();
+<<<<<<< HEAD
   const { handleSubmit, control } = useForm();
+=======
+  const { userData, setUserData } = useGlobalContext();
+  const {UserRegister} = useAuth();
+  const { handleSubmit, control, setValue, watch } = useForm();
+
+  useEffect(() => {
+    console.log("Updated userData:", userData);
+  }, [userData]);
+>>>>>>> a12ecf1 (fix PersonalInformation components)
 
   const handlerSkip = async (event) => {
     event.preventDefault();
     try {
       await UserRegister(userData);
       navigate("/path to job listing");
-    } catch (error) {
+    } catch (error){
       console.error("Error during registration", error);
     }
   };
 
   const onSubmit = async (data) => {
-    const { name, phone, birthdate, linkedin } = data;
+    const { title, jobExp, education, havefile} = data;
     setUserData({
       ...userData,
-      name,
-      phone,
-      birthdate,
-      linkedin,
+      title,
+      jobExp,
+      education,
+      havefile,
     });
 
     try {
+<<<<<<< HEAD
       await UserRegister({
         ...userData,
         name,
@@ -42,6 +55,12 @@ function PersonalInformation() {
     } catch (error) {
       console.error('Error during registration', error);
       // You can provide user feedback here, e.g., show an error message.
+=======
+      await UserRegister(userData, data);
+      navigate("/path to job listing");
+    } catch (error){
+      console.error("Error during registration", error);
+>>>>>>> a12ecf1 (fix PersonalInformation components)
     }
   };
 
@@ -52,14 +71,30 @@ function PersonalInformation() {
           You can complete this information later
         </p>
         <p className="mb-[8px] text-[10px] font-normal tracking-[1.5px] uppercase">
-          but we recommend you to do it now
+          but we reccomend you to do it now
         </p>
-        <div className="flex flex-col">
+        <div className="title-input">
           <label
-            htmlFor="name"
-            className="mb-[4px] text-[10px] font-normal tracking-[1.5px]"
+            htmlFor="title"
+            className="mb-[4px] text-xs[10px] font-normal tracking-[1.5px]"
           >
-            NAME
+            TITLE
+            <Controller
+              name="title"
+              control={control}
+              defaultValue=""
+              rules={{ required: "Title is required" }}
+              render={({ field }) => (
+                <input
+                  name="title"
+                  className="mb-[16px] flex w-[360px] h-[36px] rounded-md border border-Pink  bg-background p-[8px] text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                  id="title"
+                  type="text"
+                  placeholder="Example: Mechanical administrator"
+                  {...field}
+                />
+              )}
+            />
           </label>
           <Controller
             name="name"
@@ -83,10 +118,32 @@ function PersonalInformation() {
           {/* Add your other form fields here */}
         </div>
 
-        <div className="flex flex-row">
-          <div className="mr-[16px] w-[106px] h-[40px] px-[14px] py-[8px] border-2 border-Pink rounded-[16px] text-black text-center text-[14px] tracking-[1.25px]">
-            <button onClick={handlerSkip}>SKIP THIS!</button>
+        <div className="file-list-preview-container">
+          {watch("file") && (
+            <div className="file-preview-container">
+              <p>{watch("file").name}</p>
+              <button
+                className="file-remove-button"
+                onClick={() => setValue("file", null)}
+              >
+                x
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="mt-[16px] flex flex-row">
+          <div className="mr-[16px] w-[106px] h-[40px] px-[16px] py-[8px] bg-Pink rounded-[16px] text-white text-center text-sm tracking-[1.25px]">
+            <button onClick={() => navigate("/user/register2")}>
+              PREVIOUS
+            </button>
           </div>
+
+          <div className="text-[13px] mr-[16px] w-[106px] h-[40px] px-[16px] py-[8px] border-2 border-Pink rounded-[16px] text-black text-center tracking-[1.25px]">
+            <button onClick={handlerSkip}>
+              SKIP THIS!
+            </button>
+          </div>
+
           <div className="w-[106px] h-[40px] px-[16px] py-[8px] bg-Pink rounded-[16px] text-white text-center text-sm tracking-[1.25px]">
             <button type="submit">NEXT</button>
           </div>
@@ -96,4 +153,5 @@ function PersonalInformation() {
   );
 }
 
-export default PersonalInformation;
+export default ProfessionalInfo;
+
