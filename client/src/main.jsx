f6d298ec0975e0b341789e5931f0c71ef19be568
kkantaaa <kantavanit@gmail.com>
2023-09-12 01:21:28 +0700

    feat: added get and put method on jobRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import jwtInterceptor from "./utils/jwtInterceptor.js";
 
-jwtInterceptor();
+jwtInterceptor(); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
