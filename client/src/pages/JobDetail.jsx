088deff64a2e88f47f5e9307109498004e020e78
jashleyx <patricia.singha@outlook.com>
2023-09-12 10:45:24 +0700

    style job details banner

--- client/src/pages/JobDetail.jsx
+++ client/src/pages/JobDetail.jsx
@@ -96,7 +96,75 @@ function JobDetail() {
                 Posted 2 Days Ago
               </p>
             </div>
-            <div className="mt-[16px]">Category Type Salary</div>
+            <div className="mt-[16px] font-Montserrat flex flex-row justify-center">
+              <div className="w-[281px] h-[77px] drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] border-DarkPink border-[1px] bg-white rounded-lg flex flex-col justify-center items-center">
+                <div className="font-normal not-italic tracking-[0.15px]">
+                  Category
+                </div>
+                <div className="flex flex-row items-center text-[24px] not-italic font-normal leading-normal">
+                  <svg
+                    className="mr-[4px]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="29"
+                    viewBox="0 0 30 29"
+                    fill="none"
+                  >
+                    <path
+                      d="M12.5833 12.2174V1.20825L25.875 8.45825V25.3749H4.125V8.45825L12.5833 12.2174ZM15 5.27913V15.9366L6.54167 12.1775V22.9583H23.4583V9.89254L15 5.28033V5.27913Z"
+                      fill="#616161"
+                    />
+                  </svg>
+                  <div>Manufacturing</div>
+                </div>
+              </div>
+
+              <div className="ml-[32px] w-[281px] h-[77px] drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] border-DarkPink border-[1px] bg-white rounded-lg flex flex-col justify-center items-center">
+                <div className="font-normal not-italic tracking-[0.15px]">
+                  Type
+                </div>
+                <div className="flex flex-row items-center  text-[24px] not-italic font-normal leading-normal">
+                  <svg
+                    className="mr-[4px]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="29"
+                    viewBox="0 0 30 29"
+                    fill="none"
+                  >
+                    <path
+                      d="M21.0417 3.62492H25.875C26.1955 3.62492 26.5028 3.75222 26.7294 3.97883C26.956 4.20544 27.0833 4.51278 27.0833 4.83325V24.1666C27.0833 24.4871 26.956 24.7944 26.7294 25.021C26.5028 25.2476 26.1955 25.3749 25.875 25.3749H4.12499C3.80452 25.3749 3.49718 25.2476 3.27057 25.021C3.04396 24.7944 2.91666 24.4871 2.91666 24.1666V4.83325C2.91666 4.51278 3.04396 4.20544 3.27057 3.97883C3.49718 3.75222 3.80452 3.62492 4.12499 3.62492H8.95832V1.20825H11.375V3.62492H18.625V1.20825H21.0417V3.62492ZM24.6667 13.2916H5.33332V22.9583H24.6667V13.2916ZM18.625 6.04158H11.375V8.45825H8.95832V6.04158H5.33332V10.8749H24.6667V6.04158H21.0417V8.45825H18.625V6.04158ZM7.74999 15.7083H10.1667V18.1249H7.74999V15.7083ZM13.7917 15.7083H16.2083V18.1249H13.7917V15.7083ZM19.8333 15.7083H22.25V18.1249H19.8333V15.7083Z"
+                      fill="#616161"
+                    />
+                  </svg>
+                <div>Full time</div>
+                </div>
+              </div>
+
+              <div className="ml-[32px] w-[281px] h-[77px] drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] border-DarkPink border-[1px] bg-white rounded-lg flex flex-col justify-center items-center">
+                <div className="font-normal not-italic tracking-[0.15px]">
+                  Salary
+                </div>
+                <div className="flex flex-row items-center  text-[24px] not-italic font-normal leading-normal">
+                  <svg
+                    className="mr-[4px]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="29"
+                    viewBox="0 0 30 29"
+                    fill="none"
+                  >
+                    <path
+                      d="M15 26.5834C8.32638 26.5834 2.91667 21.1737 2.91667 14.5001C2.91667 7.82646 8.32638 2.41675 15 2.41675C21.6736 2.41675 27.0833 7.82646 27.0833 14.5001C27.0833 21.1737 21.6736 26.5834 15 26.5834ZM15 24.1667C17.5638 24.1667 20.0225 23.1483 21.8354 21.3354C23.6482 19.5226 24.6667 17.0638 24.6667 14.5001C24.6667 11.9363 23.6482 9.47757 21.8354 7.66472C20.0225 5.85186 17.5638 4.83341 15 4.83341C12.4362 4.83341 9.97749 5.85186 8.16464 7.66472C6.35179 9.47757 5.33334 11.9363 5.33334 14.5001C5.33334 17.0638 6.35179 19.5226 8.16464 21.3354C9.97749 23.1483 12.4362 24.1667 15 24.1667ZM10.7708 16.9167H17.4167C17.5769 16.9167 17.7306 16.8531 17.8439 16.7398C17.9572 16.6265 18.0208 16.4728 18.0208 16.3126C18.0208 16.1523 17.9572 15.9987 17.8439 15.8854C17.7306 15.7721 17.5769 15.7084 17.4167 15.7084H12.5833C11.7822 15.7084 11.0138 15.3902 10.4473 14.8236C9.88077 14.2571 9.56251 13.4888 9.56251 12.6876C9.56251 11.8864 9.88077 11.118 10.4473 10.5515C11.0138 9.98501 11.7822 9.66675 12.5833 9.66675H13.7917V7.25008H16.2083V9.66675H19.2292V12.0834H12.5833C12.4231 12.0834 12.2694 12.1471 12.1561 12.2604C12.0428 12.3737 11.9792 12.5273 11.9792 12.6876C11.9792 12.8478 12.0428 13.0015 12.1561 13.1148C12.2694 13.2281 12.4231 13.2917 12.5833 13.2917H17.4167C18.2178 13.2917 18.9862 13.61 19.5527 14.1765C20.1192 14.743 20.4375 15.5114 20.4375 16.3126C20.4375 17.1138 20.1192 17.8821 19.5527 18.4486C18.9862 19.0152 18.2178 19.3334 17.4167 19.3334H16.2083V21.7501H13.7917V19.3334H10.7708V16.9167Z"
+                      fill="#616161"
+                    />
+                  </svg>
+                  <p className="minsalary">2000</p>
+                  <p className="ml-1 mr-1">-</p>
+                  <p className="maxsalary">2500</p>
+                </div>
+              </div>
+            </div>
             <div className="mt-[54px]">
               <h2 className="text-[24px] font-Montserrat text-DarkPink font-normal leading-normal">
                 About Company
