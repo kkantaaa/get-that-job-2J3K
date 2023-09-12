a65fc96ee9f46da16d0951724249aa7f3ebd8734
KIKantaphong <k.kantaphong@kkumail.com>
2023-09-12 10:28:21 +0700

    fix: fix import router and fix key of router to match data sent from axios

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import authRouter from "./apps/auth.js";
 import dotenv from "dotenv";
-import registRouter from "./Website_Router/registRouter.js";
-import uploadRouter from "./Website_Router/uploadRouter.js";
-import jobRouter from "./apps/jobs.js"
+import registRouter from "./apps/registRouter.js";
+import uploadRouter from "./apps/uploadRouter.js";
+import jobRouter from "./apps/jobs.js";
 import categoryRouter from "./apps/category.js";
 import typeRouter from "./apps/type.js";
 import findthatjob from "./apps/findthatjob.js";
@@ -27,7 +27,7 @@ async function init() {
   app.use("/regist", registRouter);
   app.use("/auth", authRouter);
   app.use("/upload", uploadRouter);
-  app.use("/jobs", jobRouter)
+  app.use("/jobs", jobRouter);
   app.use("/category", categoryRouter);
   app.use("/type", typeRouter);
   app.use("/findthatjob", findthatjob);
