8ff9c291b1766202484b6852776d6aca84aa9cd3
jashleyx <patricia.singha@outlook.com>
2023-09-15 09:26:31 +0700

    fix: edit created at

--- client/src/pages/JobDetail.jsx
+++ client/src/pages/JobDetail.jsx
@@ -92,8 +92,8 @@ function JobDetail() {
             {/* created at */}
             <div className="flex flex-row uppercase justify-center">
               <img src={TimeIcon} alt="time-icon" />
-              <p className="ml-[4px] text-[10px] text-Gray font-normal tracking-[1.5px] leading-normal">
-                Posted 2 Days Ago
+              <p className="ml-[4px] mr-[4px] text-[10px] text-Gray font-normal tracking-[1.5px] leading-normal">
+                Posted at
                 {jobDetail.created_at}
               </p>
             </div>
