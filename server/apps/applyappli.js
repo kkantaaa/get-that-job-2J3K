707b6640d2e11051c75f3a00c95561ecd81738e1
KIKantaphong <k.kantaphong@kkumail.com>
2023-09-29 00:01:16 +0700

    fix(get yser data router of apply page): add/change to also fetch following detail

--- server/apps/applyappli.js
+++ server/apps/applyappli.js
@@ -92,7 +92,7 @@ applyappliRouter.get("/u/:user_id", async (req, res) => {
   console.log("user_id:", user_id);
   try {
     const userdata = await pool.query(
-      "SELECT * FROM user_profiles  WHERE user_id = $1",
+      "select * from user_profiles left join company_follows on user_profiles.user_id = company_follows.user_id where user_profiles.user_id =$1",
       [user_id]
     );
     console.log("Application submitted successfully :", userdata.rows[0]);
