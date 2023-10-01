0e49df6307a951a9b34862c3f3c2c3303d89b6f7
KIKantaphong <k.kantaphong@kkumail.com>
2023-10-01 10:29:16 +0700

    feature(recruiter register page1):add validations

--- client/src/components/RecruitLogInInfo.jsx
+++ client/src/components/RecruitLogInInfo.jsx
@@ -1,17 +1,22 @@
 import { useForm, Controller } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "@/contexts/registerContexts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ArrowRight from "../images/registration-page/arrow-right.svg";
+//
 import axios from "axios";
+//
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RecruitLogInInfo() {
+  const [emailExists, setEmailExists] = useState(false);
   const navigate = useNavigate();
   const { recruiterData, setRecruiterData } = useGlobalContext();
   const {
     handleSubmit,
     control,
-    setError, // <- เพิ่ม function setError from useForm
+    // setError, // <- เพิ่ม function setError from useForm
     formState: { errors },
   } = useForm();
 
@@ -20,57 +25,88 @@ function RecruitLogInInfo() {
   }, [recruiterData]);
 
   const onSubmit = async (data) => {
-    console.log(data);
-
-    // Check if the email already exists
+    const { companyname, companyemail, companypassword } = data;
     try {
-      // const response = await axios.post(`http://localhost:4000/recruiter?email=${data.email}`);
-      // const result = response.data;
-
-      // if (result.exists) {
-      //   setError("email", {
-      //     type: "manual",
-      //     message: "The email is already taken"
-      //   });
-      // }
-
-      // Check if passwords match
-      if (data.companypassword !== data.confirmedPassword) {
-        setError("confirmedPassword", {
-          type: "manual",
-          message: "The confirmed password does not match",
-        });
-      } else {
-        setRecruiterData({
-          company_name: data.companyname,
-          email: data.companyemail,
-          password: data.companypassword,
-        });
-
-        navigate("/recruiter/register2");
+      if (!companyname || /[^A-Za-z0-9\s]/.test(companyname)) {
+        toast.error(
+          "Company name should only contain letters and numbers and should not be empty."
+        );
+        return;
       }
+      if (
+        !/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(companyemail)
+      ) {
+        toast.error("Invalid email format");
+        return;
+      }
+      if (emailExists === true) {
+        toast.error("The email is already taken");
+        return;
+      }
+      if (
+        companypassword.length < 8 ||
+        !/^[A-Za-z0-9!@#$%^&*()_+{}\[\]:;<>,.?~_-]+$/.test(companypassword)
+      ) {
+        toast.error(
+          "Password must be at least 8 characters long and contain only normal password characters"
+        );
+        return;
+      }
+      if (data.confirmedPassword !== companypassword) {
+        console.log(data.confirmedPassword);
+        console.log(data.password);
+        toast.error("Password and password confirmation do not match");
+        return;
+      }
+
+      await setRecruiterData({
+        company_name: data.companyname,
+        email: data.companyemail,
+        password: data.companypassword,
+      });
+
+      navigate("/recruiter/register2");
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error("Error during registration", error);
+      toast.error("Error during registration");
     }
   };
 
+  const fetchRecruiterEmailFromDatabase = async (enteredEmail) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/regist/checkRecruiterDupEmail",
+        { email: enteredEmail }
+      );
+      setEmailExists(response.data.exists);
+    } catch (error) {
+      console.error("Error during registration", error);
+    }
+  };
   return (
-    <form className="font-Inter" onSubmit={handleSubmit(onSubmit)}>
-      <div className="input-container">
-        <div className="company-name-input">
-          <label
-            htmlFor="company-name"
-            className="mb-[4px] text-[10px] font-normal tracking-[1.5px]"
-          >
-            COMPANY NAME
-            <Controller
-              name="companyname"
-              control={control}
-              defaultValue=""
-              rules={{ required: "Company name is required" }}
-              render={({ field }) => (
-                <input
-                  className="mb-[16px] flex w-[360px] h-[36px] rounded-md border border-Pink  
+    <>
+      <ToastContainer
+        theme="colored"
+        closeOnClick
+        autoClose={2500}
+        position="bottom-center"
+      />
+      <form className="font-Inter" onSubmit={handleSubmit(onSubmit)}>
+        <div className="input-container">
+          <div className="company-name-input">
+            <label
+              htmlFor="company-name"
+              className="mb-[4px] text-[10px] font-normal tracking-[1.5px]"
+            >
+              COMPANY NAME
+              <Controller
+                name="companyname"
+                control={control}
+                defaultValue=""
+                rules={{ required: "Company name is required" }}
+                render={({ field }) => (
+                  <input
+                    className="mb-[16px] flex w-[360px] h-[36px] rounded-md border border-Pink  
                   bg-background p-[8px] text-[14px] placeholder:text-muted-foreground"
                   name="companyname"
                   id="companyname"
@@ -101,19 +137,24 @@ function RecruitLogInInfo() {
                 <input
                   className="mb-[16px] flex w-[360px] h-[36px] rounded-md border border-Pink  
                   bg-background p-[8px] text-[14px] placeholder:text-muted-foreground"
-                  id="companyemail"
-                  name="companyemail"
-                  type="email"
-                  placeholder="some.user@mail.com"
-                  {...field}
-                />
-              )}
-            />
-          </label>
-          <div className="text-red-500 text-[10px] uppercase font-bold tracking-[0.25px]">
-            {errors.companyemail && errors.companyemail.message}
+                    id="companyemail"
+                    name="companyemail"
+                    type="email"
+                    placeholder="some.user@mail.com"
+                    {...field}
+                    onChange={(e) => {
+                      const enteredEmail = e.target.value;
+                      fetchRecruiterEmailFromDatabase(enteredEmail);
+                      field.onChange(e);
+                    }}
+                  />
+                )}
+              />
+            </label>
+            <div className="text-red-500 text-[10px] uppercase font-bold tracking-[0.25px]">
+              {errors.companyemail && errors.companyemail.message}
+            </div>
           </div>
-        </div>
 
         <div className="password-input">
           <label
@@ -174,14 +215,15 @@ function RecruitLogInInfo() {
           </div>
         </div>
 
-        <div className="ml-[127px] w-[106px] h-[40px] px-[16px] py-[8px] active:bg-DarkPink hover:bg-LightPink bg-Pink rounded-[16px] text-white leading-[24px] font-[500px] text-[14px] tracking-[1.25px]">
-          <button className="flex flex-row" type="submit">
-            <div className="ml-[10px]">NEXT</div>
-            <img src={ArrowRight} />
-          </button>
+          <div className="ml-[127px] w-[106px] h-[40px] px-[16px] py-[8px] active:bg-DarkPink hover:bg-LightPink bg-Pink rounded-[16px] text-white leading-[24px] font-[500px] text-[14px] tracking-[1.25px]">
+            <button className="flex flex-row" type="submit">
+              <div className="ml-[10px]">NEXT</div>
+              <img src={ArrowRight} />
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 }
 
